adc1208c1e11741e62b790c900cdc62c1b6361ed
Enock <tshims.enock31@gmail.com>
2026-01-23 23:11:11 -0500

    .

--- Exercises/GenericPraticeExercise3 SimpleStack/GenericPraticeExercise3 SimpleStack/Program.cs
+++ Exercises/GenericPraticeExercise3 SimpleStack/GenericPraticeExercise3 SimpleStack/Program.cs
@@ -1,33 +1,5 @@
 ﻿namespace GenericPraticeExercise3_SimpleStack;
 
-public class MyStack<T>
-{
-    private readonly List<T> _item = new();
-
-    public int Count => _item.Count;
-
-    public void Push(T item)
-    {
-        if (item is null)
-        {
-            throw new ArgumentNullException(nameof(item), "Cannot be null");
-        }
-        _item.Add(item);
-    }
-
-    public T Pop()
-    {
-        if (_item.Count == 0)
-        {
-            throw new InvalidOperationException("Nothing to pop");
-        }
-        int lastIndex = _item.Count - 1;
-        T item = _item[lastIndex];
-        _item.RemoveAt(lastIndex);
-        return item;
-    }
-}
-
 internal class Program
 {
     static void Main(string[] args)
