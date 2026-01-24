88230729507a09492e93a5f1dd45a81c7ffda31c
Enock <tshims.enock31@gmail.com>
2026-01-23 19:15:40 -0500

    simple generic exercise

---
+++ Exercises/GenericPraticeExercise1/GenericPraticeExercise1/Box.cs
@@ -0,0 +1,10 @@
+﻿namespace GenericPraticeExercise1;
+
+public class Box<T>
+{
+    private T _value = default!;
+
+    public T SetValue(T value) => _value = value;
+
+    public T GetValue() => _value;
+}
---
+++ Exercises/GenericPraticeExercise1/GenericPraticeExercise1/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace GenericPraticeExercise1;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var box = new Box<int>();
+        box.SetValue(42);
+
+        Console.WriteLine(box.GetValue());
+    }
+}
