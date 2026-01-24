68055fab3d73995ea05bcdc5875ed1cb09b6b594
Enock <tshims.enock31@gmail.com>
2026-01-24 17:54:32 -0500

    Handling Exeptions

---
+++ Learning/Exeption Handling/Exeption Handling/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Exeption_Handling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var caculator = new Caculator();
+                var result = caculator.divide(5, 0);
+            }
+            catch (DivideByZeroException ex) { }
+
+            catch (ArithmeticException)
+            {
+
+                Console.WriteLine("Cannot divid by zero");
+            }
+
+            catch (Exception ex) { }
+
+            finally { Console.WriteLine(); }
+
+
+        }
+    }
+
+    internal class Caculator
+    {
+        public int divide(int x, int y)
+        {
+            return x / y;
+        }
+    }
+}
