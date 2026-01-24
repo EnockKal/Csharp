83c976cc973024296ce3ac85264548697d25271d
Enock <tshims.enock31@gmail.com>
2026-01-23 19:42:35 -0500

    more basic generic ecercises

---
+++ Exercises/GenericPraticeExercise2/GenericPraticeExercise2/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace GenericPraticeExercise2;
+
+
+// Exercise: Swap two value
+internal class Program
+{
+
+    static void Swap<T>(ref T a, ref T b)
+    {
+        T temp = a;
+        a = b;
+        a = temp;
+    }
+
+    static void Main(string[] args)
+    {
+        int a = 1;
+        int b = 2;
+        Console.WriteLine($"A = {a}");
+        Console.WriteLine($"B = {b}");
+
+        Console.WriteLine();
+
+        Swap(ref a, ref b);
+        Console.WriteLine($"A = {a}");
+        Console.WriteLine($"B = {b}");
+    }
+}
