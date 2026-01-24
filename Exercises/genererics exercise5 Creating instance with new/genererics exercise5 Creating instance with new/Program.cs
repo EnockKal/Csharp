e995be6159389cbc2c7e227686ea10e479ed83c3
Enock <tshims.enock31@gmail.com>
2026-01-24 00:05:53 -0500

    Generic exercise Contrain new ( )

---
+++ Exercises/genererics exercise5 Creating instance with new/genererics exercise5 Creating instance with new/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace genererics_exercise5_Creating_instance_with_new;
+
+public class ObjectFactory<T> where T : new()
+{
+    public T Create() => new T();
+}
+
+public class Person
+{
+
+}
+
+public class Car
+{
+    public Car(string model) // Non empty constructor
+    {
+    }
+}
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var factory = new ObjectFactory<Person>();
+        Person p = factory.Create();
+
+        Console.WriteLine(p.GetType().Name);
+
+        //var car = new ObjectFactory<Car>(); // doesn't work bcz the constructor of Car isn't empty
+    }
+}
