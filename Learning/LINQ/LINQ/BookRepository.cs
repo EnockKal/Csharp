88ea73a59a0dae5385865afa9248b6f5d3a7f7ec
Enock <tshims.enock31@gmail.com>
2026-01-24 14:58:14 -0500

    basic of LINQ

---
+++ Learning/LINQ/LINQ/BookRepository.cs
@@ -0,0 +1,16 @@
+﻿namespace LINQ;
+
+public class BookRepository
+{
+    public IEnumerable<Book> GetBooks()
+    {
+        return new List<Book>
+        {
+            new Book() { Title = "ado.net step by step", Price = 5},
+            new Book() { Title = "ASP.NET MVC", Price = 9.99f},
+            new Book() { Title = "ASP.NET Web API", Price = 12},
+            new Book() { Title = "C# Advanced topics", Price = 7},
+            new Book() { Title = "C# Advanced topics", Price = 9},
+        };
+    }
+}
---
+++ Learning/LINQ/LINQ/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace LINQ;
+
+internal class Program
+{
+    /*
+     * Filter: Where
+     * Transform or project: Select
+     * Sort: OrderBy, ThenBy, OrderByDescending
+     * Check: Any, All
+     * Get one: FirstOrDefault, SingleOrDefault (use Single* only when you expect exactly one)
+     * Aggregate: Count, Sum, Average, Min, Max
+     * Group: GroupBy
+     * Combine: Join, Concat, Union, Intersect, Except
+     * Flatten: SelectMany
+     * Paging: Skip, Take
+     */
+
+    static void Main(string[] args)
+    {
+        var books = new BookRepository().GetBooks();
+        var cheapBooks = books
+                            .Where(b => b.Price < 10)
+                            .OrderBy(b => b.Title)
+                            .Select(b => b.Title);
+
+
+        foreach (var book in cheapBooks)
+        {
+            //Console.WriteLine($"{book.Title} {book.Price}");
+            Console.WriteLine(book);
+        }
+    }
+}
